2ef020fd6e13dbb3c0fb859e9b613cd0bf113a00
shigon255 <ray5233512@gmail.com>
2024-04-03 08:15:10 +0800

    0815

--- Lab2/check_mflops.py
+++ Lab2/check_mflops.py
@@ -2,8 +2,10 @@ import torch
 import torch_pruning as tp
 from torchvision.models import mobilenet_v2
 
-model = torch.load('./model/mobilenet.pth', map_location="cpu")
-example_inputs = torch.zeros(1, 3, 224, 224).to(args.device)
+model = torch.load('./mobilenet_61_90.pth', map_location="cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+example_inputs = torch.zeros(1, 3, 224, 224).to(device)
 ops, size = tp.utils.count_ops_and_params(model, example_inputs=example_inputs)
 
 MFLOPs = ops/1e6
